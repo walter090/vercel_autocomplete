297cd8309ee068b69176a5bc869e9fc40de1b810
walter090 <walter.wu090@gmail.com>
2025-07-23 14:29:02 -0700

    Commit changes and improvements

--- src/app/_components/search-action.ts
+++ src/app/_components/search-action.ts
@@ -3,16 +3,16 @@
 import {parseTextFile} from "@/app/_utils/parser";
 
 export async function search(searchText: string): Promise<string[]> {
-  // changed to fetch word list from server action, avoiding passing it as a prop and arg
+  // CHANGED to fetch word list from server action, avoiding passing it as a prop and arg
   const wordList = parseTextFile();
 
   if (!searchText) {
     return [];
   }
 
-  const lowerCaseSearchText = searchText.toLowerCase();
-  const results = wordList.filter(word => word.toLowerCase().includes(lowerCaseSearchText));
   return new Promise((resolve) => {
-      resolve(results);
+    const lowerCaseSearchText = searchText.toLowerCase();
+    const results = wordList.filter(word => word.toLowerCase().includes(lowerCaseSearchText));
+    resolve(results.slice(0, 10)); // CHANGED Return only the first 10 results
   });
 }
